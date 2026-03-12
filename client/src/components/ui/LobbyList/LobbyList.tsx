aadc3026459ed17c6268ac81d06b89c655c8ce9f
Zerosity <75740714+Zerosity@users.noreply.github.com>
2026-03-12 12:26:39 -0400

    add: LobbyList gap styling

--- client/src/components/ui/LobbyList/LobbyList.tsx
+++ client/src/components/ui/LobbyList/LobbyList.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Socket } from 'socket.io-client';
 import CornerCard from "../CornerCard/CornerCard.tsx";
-import { Text } from "@mantine/core";
+import {Stack, Text} from "@mantine/core";
 import LobbyCard from '../LobbyCard/LobbyCard.tsx';
 
 interface LobbyListProps {
@@ -44,22 +44,28 @@ export default function LobbyList({ socket }: LobbyListProps) {
             >
                 Open Lobbies
             </Text>
-            {lobbies.length === 0 ? (
-                <Text
-                  style={{
-                    color: 'rgba(200,184,122,0.3)',
-                    fontSize: 13,
-                    textAlign: 'center',
-                    padding: '40px 0'
-                  }}
-                >
-                    No Open Lobbies Available
-                </Text>
-            ) : (
-                lobbies.map((lobby) => (
-                  <LobbyCard key={lobby.code} lobby={lobby} />
-                ))
-            )}
+            <Stack
+                style={{
+                    gap: '10px',
+                }}
+            >
+                {lobbies.length === 0 ? (
+                    <Text
+                        style={{
+                            color: 'rgba(200,184,122,0.3)',
+                            fontSize: 13,
+                            textAlign: 'center',
+                            padding: '40px 0'
+                        }}
+                    >
+                        No Open Lobbies Available
+                    </Text>
+                ) : (
+                    lobbies.map((lobby) => (
+                        <LobbyCard key={lobby.code} lobby={lobby} />
+                    ))
+                )}
+            </Stack>
         </CornerCard>
     );
 }
